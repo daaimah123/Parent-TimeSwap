cff9c0ea1e3d5c2da7326c7433dc2d26717851e9
daaimah123 <41805952+daaimah123@users.noreply.github.com>
2019-04-18 14:38:07 -0700

    remove react class from firebase/index.js to use firebase/firebase.js class, pull the class into top level

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,20 +1,8 @@
-import React from 'react';
-
 //importing the .createContext() with two component default options set to null
 import FirebaseContext from './context';
-//importing the class
+//importing the class created in firebase file
 import Firebase from './firebase';
 
-class Firebase extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Firebase</h1>
-            </div>
-        )
-    }
-}
-
 export default Firebase;
-//knows to go to the top level, will need to place Firebase class next to it for use
+//knows to go to the top level
 export { FirebaseContext };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import { FirebaseContext } from './components/Firebase';
 import Firebase from './components/Firebase/firebase';
 
 ReactDOM.render(
